827e429b926e3bd88912b76cc7dc78d1cabcf3f6
Cassius Samuel <cassiusejekwu@gmail.com>
2026-02-04 19:55:58 +0100

    fix

--- src/components/GridEffect.tsx
+++ src/components/GridEffect.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import { Darker_Grotesque } from "next/font/google";
+
 interface EnhancedGridProps {
   className?: string;
   size?: number
 }
 
 const EnhancedGrid: React.FC<EnhancedGridProps> = ({ className = "", size=40 }) => {
+
+  const {theme} = useTheme()
+
   return (
     <div className={`fixed h-[300vh] inset-0  -z-10 overflow-hidden ${className}`}>
       {/* Grid pattern */}
@@ -13,8 +19,8 @@ const EnhancedGrid: React.FC<EnhancedGridProps> = ({ className = "", size=40 }) 
         className="absolute inset-0"
         style={{
           backgroundImage: `
-            linear-gradient(rgba(255, 255, 255, 0.05) 1px, transparent 1px),
-            linear-gradient(90deg, rgba(255, 255, 255, 0.05) 1px, transparent 1px)
+            linear-gradient(${theme === 'dark' ? "rgba(255, 255, 255, 0.05)" : "rgba(0, 0, 0, 0.05)"} 1px, transparent 1px),
+            linear-gradient(90deg, ${theme === 'dark' ? "rgba(255, 255, 255, 0.05)" : "rgba(0, 0, 0, 0.05)"}  1px, transparent 1px)
           `,
           backgroundSize: `${size}px ${size}px `,
         }}
@@ -24,8 +30,7 @@ const EnhancedGrid: React.FC<EnhancedGridProps> = ({ className = "", size=40 }) 
       <div
         className="absolute inset-0"
         style={{
-          background: `
-            radial-gradient(circle at 50% 50%, transparent 0%, rgba(0, 6, 15, 0.8) 100%)
+          background:  `radial-gradient(circle at 50% 50%, transparent 0%, ${theme === 'dark' ? "rgba(0, 6, 15, 0.8)" : "rgba(255, 255, 240, 0.8)"} 100%)
           `,
         }}
       />
